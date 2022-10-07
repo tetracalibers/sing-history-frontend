30f11b99700c3470cbfc285f9847dae86bf511bc
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-10-07 11:22:29 +0900

    @apollo/client導入設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
+import type { AppProps } from "next/app"
+
+const cache = new InMemoryCache()
+const client = new ApolloClient({
+  uri: `${process.env.NEXT_PUBLIC_BACKEND_URL}/graphiql`,
+  cache,
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
