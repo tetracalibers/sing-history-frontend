6d2f26b0de46cdd1313e2b85700381b5af25698c
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-10-07 17:39:20 +0900

    index.tsxの不要なコード削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,5 @@
 import type { NextPage } from "next"
-import { gql, useQuery } from "@apollo/client"
 import { SetlistTable } from "../components/domain/SetlistTable"
-
-const SETLIST_QUERY = gql`
-  query {
-    setlist {
-      artistName
-      songName
-      id
-    }
-  }
-`
-
-type Setlist = {
-  artistName: string
-  songName: string
-  id: string
-}
-
-type SetlistResponse = {
-  setlist: Setlist[]
-}
 
 const Home: NextPage = () => {
   return <SetlistTable />
