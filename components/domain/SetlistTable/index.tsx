a5bc15801dc761c633af7ee3ba74bbcc66575554
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-10-08 20:11:25 +0900

    背景のスタイリング

--- components/domain/SetlistTable/index.tsx
+++ components/domain/SetlistTable/index.tsx
@@ -4,6 +4,16 @@ import { useVirtualScroll } from "./useVirtualScroll.hook"
 import { memo, useRef } from "react"
 import { useWindowSize } from "../../../hooks/useWindowSize.hook"
 
+const _TwinkleBack = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: url("/twinkleBack.jpg");
+  background-blend-mode: screen;
+  background-size: cover;
+`
+
+const TwinkleBack = memo(_TwinkleBack)
+
 const _VirtualRoot = styled.div`
   --bg-color: #f7f9ff;
   --shadow-color: #b1b2ff;
@@ -32,34 +42,13 @@ const _VirtualRoot = styled.div`
     background-clip: content-box;
   }
 
-  /* scroll hint */
-  background: linear-gradient(var(--bg-color) 33%, rgba(255, 255, 255, 0)),
-    linear-gradient(rgba(255, 255, 255, 0), var(--bg-color) 66%) 0 100%,
-    radial-gradient(
-      farthest-side at 50% 0,
-      var(--shadow-color),
-      rgba(255, 255, 255, 0)
-    ),
-    radial-gradient(
-        farthest-side at 50% 100%,
-        var(--shadow-color),
-        rgba(255, 255, 255, 0)
-      )
-      0 100%;
-  background-color: var(--bg-color);
-  background-repeat: no-repeat;
-  background-attachment: local, local, scroll, scroll;
-  background-size: 100% 33px, 100% 33px, 100% 11px, 100% 11px;
-
   /* design */
-  width: 100%;
   list-style: none;
-  box-shadow: rgba(0, 0, 0, 0.1) -4px 9px 25px -6px;
-  border-radius: 10px;
-  padding: 0;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
+  background-image: linear-gradient(
+    221deg,
+    rgba(12, 186, 186, 0.9) 0%,
+    rgba(100, 125, 238, 0.9) 74%
+  );
 `
 
 const _VirtualScrollArea = styled.div`
@@ -88,17 +77,19 @@ const _SetlistTable = () => {
   })
 
   return (
-    <VirtualRoot ref={rootRef} style={{ maxHeight: wHeight }}>
-      <VirtualScrollArea>
-        <Ul style={ulStyle}>
-          {renderItems.map(node => (
-            <li style={{ height: vItemHeight }} key={node.id}>
-              <Song song={node} />
-            </li>
-          ))}
-        </Ul>
-      </VirtualScrollArea>
-    </VirtualRoot>
+    <TwinkleBack>
+      <VirtualRoot ref={rootRef} style={{ maxHeight: wHeight }}>
+        <VirtualScrollArea>
+          <Ul style={ulStyle}>
+            {renderItems.map(node => (
+              <li style={{ height: vItemHeight }} key={node.id}>
+                <Song song={node} />
+              </li>
+            ))}
+          </Ul>
+        </VirtualScrollArea>
+      </VirtualRoot>
+    </TwinkleBack>
   )
 }
 
